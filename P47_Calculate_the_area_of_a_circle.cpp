1c92b7c5a65a190b30de0928ac4726f0092ae88a
rajajainds <rajajainds@gmail.com>
2026-04-23 19:03:12 +0530

    Program_47

---
+++ P47_Calculate_the_area_of_a_circle.cpp
@@ -0,0 +1,14 @@
+// Calculate the area of a circle
+#define pi 3.1425
+#include<iostream>
+using namespace std;
+
+int main(){
+    float radius = 34.0;
+    float area = pi*radius*radius;
+
+    cout<<"The areas of the circle is "<<area;
+
+
+    return 0;
+}
