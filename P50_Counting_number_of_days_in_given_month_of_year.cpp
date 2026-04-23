1d8a7d9407d163c79e90221c31527e10890037ba
rajajainds <rajajainds@gmail.com>
2026-04-23 19:16:58 +0530

    Program_50

---
+++ P50_Counting_number_of_days_in_given_month_of_year.cpp
@@ -0,0 +1,25 @@
+// Counting number of days in a given month of a year
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int month = 11, year=2013;
+    
+    if((month==2) && ((year%400==0) || ((year%100!=0)&&(year%4==0)))){
+        cout<<"Number of days is 29";
+    }
+    else if(month==2){
+        cout<<"Number of days is 28";
+    }
+    else if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+        cout<<"Number of days is 31";
+    }
+    else if(month==4 || month==6 || month==9 || month==11){
+        cout<<"Number of days is 30";
+    }
+    else cout<<"Invalid month";
+    
+    return 0;
+}
