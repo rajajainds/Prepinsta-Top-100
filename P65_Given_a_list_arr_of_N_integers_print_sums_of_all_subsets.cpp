44977c48517cace6c47679f8e917d48497426842
rajajainds <rajajainds@gmail.com>
2026-04-26 17:44:48 +0530

    Program_65

---
+++ P65_Given_a_list_arr_of_N_integers_print_sums_of_all_subsets.cpp
@@ -0,0 +1,25 @@
+// Given a list arr of N integers, print sums of all subsets
+
+#include<iostream>
+using namespace std;
+ 
+void subsetSum(int arr[], int l, int r, int sum = 0)
+{
+    if (l > r) {
+        cout << sum << " ";
+        return;
+    }
+ 
+    subsetSum(arr, l + 1, r, sum + arr[l]);
+ 
+    subsetSum(arr, l + 1, r, sum);
+}
+ 
+int main()
+{
+    int arr[] = { 4, 3, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    subsetSum(arr, 0, n - 1);
+    return 0;
+}
