f4cb38d5b23774faf3a106c4b72743d373f80ac7
rajajainds <rajajainds@gmail.com>
2026-04-29 13:16:12 +0530

    Program_76

---
+++ P76_Find_Second_Smallest_Element_in_an_Array.cpp
@@ -0,0 +1,35 @@
+// Find Second Smallest Element in an Array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]={32,54,12,34,56,76,32};
+
+    int smallest = arr[0];
+    
+    for(int s:arr){
+        if(smallest>s){
+            smallest=s;
+        }
+    }
+
+    for(int s:arr){
+        if (smallest=s){
+           continue;
+        }
+        if(smallest>s){
+            smallest=s;
+        }
+        
+    }
+    
+    cout<<"The smallest element in array is "<<smallest<<endl;
+
+
+    return 0;
+}
+
+
+
