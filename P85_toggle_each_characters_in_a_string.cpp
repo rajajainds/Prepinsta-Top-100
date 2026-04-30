a1a364128f48632fc285d06a4e86fbff962dfbc8
rajajainds <rajajainds@gmail.com>
2026-04-30 18:53:03 +0530

    Program_85

---
+++ P85_toggle_each_characters_in_a_string.cpp
@@ -0,0 +1,32 @@
+// toggle each characters in a string
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+ 
+    char str[100];
+    int i;
+  	
+   
+    cout<<"Enter the String: ";
+    gets(str);
+  	
+
+  	for (i = 0; str[i]!='\0'; i++)
+  	{
+  	   
+  	    if(str[i] >= 'A' && str[i] <= 'Z') { str[i] = str[i] + 32; } else if(str[i] >= 'a' && str[i] <= 'z')
+            {
+  	      str[i] = str[i] - 32;
+	    }		
+  		
+  	}
+
+  	cout<<"\nToggled string: "<<str<<endl; 
+  	
+  	return 0;
+}
+
