dbe2a94e4e1dc1ef8a07a67e9876ca37bd05d42f
rajajainds <rajajainds@gmail.com>
2026-04-30 18:43:23 +0530

    Program_83

---
+++ P83_print_ASCII_value_of_a_character.cpp
@@ -0,0 +1,14 @@
+// print ASCII value of a character
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char c='A';
+
+    cout<<"Tha ascii value of "<<c<<" is "<<(int)(c)<<endl;
+
+
+    return 0;
+}
