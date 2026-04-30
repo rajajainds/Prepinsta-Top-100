d80c9565f89b2fd2741bfd13dc2631dff15ef5bb
rajajainds <rajajainds@gmail.com>
2026-04-30 18:39:18 +0530

    Program_82

---
+++ P82_Check_whether_a_character_is_a_alphabet_or_not.cpp
@@ -0,0 +1,20 @@
+// Check whether a character is a alphabet or not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    char c='4';
+
+    if(isalpha(c)){
+        cout<<c<<" is a character";
+    }
+     else{
+        cout<<c<<" is not a character";
+    }
+
+return 0;
+
+}
