a8b71ed48e9dcd8ee4554d648f7d400e863add00
rajajainds <rajajainds@gmail.com>
2026-05-02 19:06:23 +0530

    Program_93

---
+++ P93_Calculate_frequency_of_characters_in_a_string.cpp
@@ -0,0 +1,27 @@
+// Calculate frequency of characters in a string
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+    char str[100]="rajajain";
+    int i;
+    int freq[256] = {0};
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+    
+
+    for(i = 0; i < 256; i++)
+    {
+        if(freq[i] != 0)
+        {
+           cout<<"The frequency of "<<char(i)<<" is "<<freq[i]<<endl;
+        }
+    }
+    return 0;
+}
